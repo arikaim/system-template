6960009776b15f71c4b4438f15fd5d14afde8fe6
arikaim <sales@intersoft.biz>
2021-02-28 13:54:48 +0200

    =

--- components/admin/system/info/info.js
+++ components/admin/system/info/info.js
@@ -3,4 +3,11 @@
 arikaim.component.onLoaded(function() {  
     $('#php_extensions_info').accordion();
     $('#composer_packages_info').accordion();
+
+    arikaim.ui.button('.check-console',function(element) {
+        arikaim.page.loadContent({
+            id: 'console_content',
+            component: 'system:admin.system.info.console'
+        });
+    });
 });
