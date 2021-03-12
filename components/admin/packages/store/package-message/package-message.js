20081e8d4818095618859045c6be219600c322ce
arikaim <sales@intersoft.biz>
2021-03-12 20:30:21 +0200

    =

---
+++ components/admin/packages/store/package-message/package-message.js
@@ -0,0 +1,19 @@
+'use strict';
+
+arikaim.component.onLoaded(function() {
+    arikaim.ui.button('.open-package-download',function(element) {
+        var contentId = $(element).attr('content-id');
+        var type = $(element).attr('package-type');
+
+        console.log(type);
+
+        return arikaim.page.loadContent({
+            id: contentId,           
+            component: 'system:admin.packages.store.view',
+            params: { 
+                type: type                
+            }
+        },function(result) {                   
+        });
+    });
+});
